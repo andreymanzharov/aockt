4319322451229395c6682bf485509f3c26a7afd0
Andrey Manzharov <amanzharov@yandex.ru>
2022-12-01 15:02:58 +0300

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun calories(input: List<String>): Sequence<Int> {
+        val it = input.iterator()
+        return generateSequence {
+            var sum: Int? = null
+            while (it.hasNext()) {
+                val next = it.next().takeUnless(String::isEmpty)?.toInt() ?: break
+                sum = (sum ?: 0) + next
+            }
+            sum
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = calories(input).max()
+
+    fun part2(input: List<String>): Int = calories(input).toList().sorted().takeLast(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
